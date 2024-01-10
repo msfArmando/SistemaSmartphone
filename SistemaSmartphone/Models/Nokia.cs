00684f305cda77c68c0ceb984976476aa87b4e80
msfArmando-github <armandomonsaof@gmail.com>
2024-01-10 14:05:33 -0300

    Add project files.

---
+++ SistemaSmartphone/Models/Nokia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaSmartphone.Models
+{
+    public class Nokia : Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria {  get; set; }
+
+        public Nokia(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+            Numero = numero; 
+            Modelo = modelo;
+            Imei = iMEI;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia");
+        }
+    }
+}
---
+++ SistemaSmartphone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SistemaSmartphone.Models;
+
+namespace SistemaSmartphone{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", iMEI: "11111111", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone IPhone:");
+            Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", iMEI: "22222222", memoria: 128);
+            nokia.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
